ce480108578d3f3f4ae45889cbd3c0dc6da173e2
viccjiang <binbinbb520@gmail.com>
2021-08-25 19:10:45 +0800

    fix swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,27 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.mySwiper', {
+  // slidesPerColumnFill: 'horizontal',
+  slidesPerView: 2,
+  grid: {
+    fill: 2,
+    rows: 2,
+  },
+  spaceBetween: 30,
+
+  breakpoints: {
+
+    768: {
+      slidesPerView: 2,
+      grid: {
+        fill: 2,
+        rows: 2,
+      },
+    },
+
+    992: {
+      slidesPerView: 1.5,
+      grid: {
+        rows: 1,
+      },
+    },
+  },
 });

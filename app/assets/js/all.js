d7bf3985b642a5d6867168f4c21f28276eac0bae
viccjiang <binbinbb520@gmail.com>
2021-08-26 14:19:41 +0800

    add index custon swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,7 +2,7 @@ var swiper = new Swiper('.mySwiper', {
   // slidesPerColumnFill: 'horizontal',
   slidesPerView: 2,
   grid: {
-    fill: 2,
+    fill: 'row',
     rows: 2,
   },
   spaceBetween: 30,
@@ -12,7 +12,7 @@ var swiper = new Swiper('.mySwiper', {
     768: {
       slidesPerView: 2,
       grid: {
-        fill: 2,
+        fill: 'row',
         rows: 2,
       },
     },
@@ -21,6 +21,36 @@ var swiper = new Swiper('.mySwiper', {
       slidesPerView: 1.5,
       grid: {
         rows: 1,
+      },
+    },
+  },
+});
+
+
+
+var customswiper = new Swiper('.customswiper', {
+  // slidesPerColumnFill: 'horizontal',
+  slidesPerView: 1,
+  grid: {
+    fill: 1,
+    rows: 3,
+  },
+  spaceBetween: 30,
+
+  breakpoints: {
+
+    768: {
+      slidesPerView: 2,
+      grid: {
+        fill: 'row',
+        rows: 2,
+      },
+    },
+
+    992: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
       },
     },
   },

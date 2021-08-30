eec9e0457610a29d5e87599dc1cd1d901f95b101
viccjiang <binbinbb520@gmail.com>
2021-08-30 17:35:29 +0800

    Update 2021-08-30T09:35:17.661Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -48,6 +48,29 @@ var customswiper = new Swiper('.customswiper', {
     }
   }
 });
+var customswiper = new Swiper('.course-swiper', {
+  // slidesPerColumnFill: 'horizontal',
+  slidesPerView: 1,
+  grid: {
+    fill: 1,
+    rows: 4
+  },
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      grid: {
+        rows: 1
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      grid: {
+        rows: 1
+      }
+    }
+  }
+});
 var elem = document.querySelector('input[name="datepicker"]');
 var datepicker = new Datepicker(elem, {
   autohide: true,
